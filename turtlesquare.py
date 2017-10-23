2a0e96ff661c1c45de68a3810b4470a72c875646
student <student@USProgrammingCart6.local>
2017-10-23 13:40:50 -0400

    turtle

---
+++ turtlesquare.py
@@ -0,0 +1,20 @@
+import turtle
+
+window =  turtle.Screen()
+
+nick = turtle.Turtle()
+anu = turtle.Turtle()
+nick.speed(10)
+
+nick.forward(250)
+nick.right(90)
+nick.forward(250)
+nick.right(90)
+nick.forward(250)
+nick.right(90)
+nick.forward(250)
+
+anu.speed(0)
+anu.circle(150)
+
+window.exitonclick()
