08634720f9e9b8e326519d568a769b0ab2981a9c
student <student@USProgrammingCart7.local>
2017-11-15 14:19:43 -0500

    11/15/17

--- student.py
+++ student.py
@@ -11,52 +11,55 @@ class Student:
         self.height = height
 
     def disstu(self):
-        print "Students name is", self.name
-        print "Students favorite subject is", self.favsub
+        print "Student's name is", self.name
+        print "Student's favorite subject is", self.favsub
         print "Student's age is ", self.age
         print "Student's height is", self.height
 
     def stunum(self):
         Student.count += 1
-        print "Students ID number is", Student.count
+        print "Student's ID number is", Student.count
 
 
     def which(self):
-        if tech == "apple":
-            Student.apple +=1
+        if tech == "samsung":
+            Student.samsung +=1
         else:
-            Student.samsung += 1
+            Student.apple += 1
+        print "Student chose", tech
 
-
-
-while 1:
+end = 1
+while end == 1:
     x =raw_input("do you want to add a person to the directory? (y? or n?)")
     if x == "y":
-        name = raw_input("What is the students name?")
-        height = raw_input("What is the students height?")
-        age = raw_input("What is the students age?")
-        favsub = raw_input("What is the students favorite subject?")
-        tech = raw_input("Apple or samsung?")
+        name = raw_input("What is the student's name? ")
+        height = raw_input("What is the student's height? ")
+        age = raw_input("What is the student's age? ")
+        favsub = raw_input("What is the student's favorite subject? ")
+        tech = raw_input("Apple or samsung? ")
         directory.append(Student(name, favsub, age, height))
         student1 = Student(name, favsub, age, height)
-        print "-----------"
-        print "Student's Information:"
-        student1.disstu()
-        print "Student's ID number:"
         student1.stunum()
         student1.which()
-
+        print "_______________"
     else:
-        break
+        n = raw_input("Do you want to see a list of students and their information? (y? or n?)")
+        if n == "y":
+            print "People in the directory:"
+            for person in directory:
+                print "Name:", person.name
+                print "Height:", person.height
+                print "Age:", person.age
+                print "Favorite subject:", person.favsub
+                print "Apple or Samsung:", person.which()
+                print person.stunum()
+                print "_________________"
+        else:
+            z = raw_input("Do you want to leave the program? (y? or n?)")
+            if z == "y":
+                end = 2
 
+print "_________________"
+print "The final technology debate total is:"
 print Student.apple, "student(s) has/have apple."
 print Student.samsung, "student(s) has/have samsung."
-
-print "People in the directory:"
-for person in directory:
-    print "Name:", person.name
-    print "Height:", person.height
-    print "Age:", person.age
-    print "Favorite subject:", person.favsub
-    print "Apple or Samsung:", person.which
-    print "---------"
