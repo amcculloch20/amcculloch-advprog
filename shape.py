14716208b22953be4b83d32ea3190bb359a2a03d
student <student@USProgrammingCart1.local>
2017-11-29 14:20:43 -0500

    11/29/17

--- shape.py
+++ shape.py
@@ -17,16 +17,48 @@ class Rect():
         self.y = w
 
     def area(self):
-        return x*y
+        return self.x * self.y
 
     def perimeter(self):
-        return 2*y + 2*x
+        return 2*self.y + 2*self.x
 
     def __str__(self):
         return "Rectangle has a length of %.2f and a width of %.2f. It has an area of %.2f and a perimeter of %.2f" %(self.x, self.y, self.area(), self.perimeter())
 
+class Square(Rect):
+    def __init__(self, x):
+        self.x = x
+        self.y = x
+
+    def __str__(self):
+        return "Square has an area of %.2f and a perimeter of %.2f" %(self.area(), self.perimeter())
+
+class Rtri():
+    def __init__(self, b,h):
+        self.x = b
+        self.y = h
+
+    def area(self):
+        return (1/2)*self.x*self.y
+
+    def hypo(self):
+        return (self.x**2 + self.y**2)**(1/2)
+
+    def perimeter(self):
+        return self.x + self.y + self.hypo()
+
+    def __str__(self):
+        return "This right triangle has an area of %.2f and a perimeter of %.2f and the hypotenuse of %.2f" %(self.area(), self.perimeter(), self.hypo())
+
 circle1 = Circle(5)
 rect1 = Rect(3,2)
+square1 = Square(4)
+rtri1 = Rtri(3,4)
 
 print circle1
+print "----------------------"
 print rect1
+print "----------------------"
+print square1
+print "----------------------"
+print rtri1
